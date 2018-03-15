82f9316b557a808875fe90076785a3298e3d3595
Vitor Mendonca <vitor.vfm@gmail.com>
2018-03-15 00:31:27 +0000

    Do batching manually
    This works around a problem where the neural network spawns thousands of
    threads and exhausts system resources

--- top_bottom_nn.py
+++ top_bottom_nn.py
@@ -12,28 +12,35 @@ RESCALE_SIZE = [224, 224]
 SAMPLE_CATEGORY_IMG_FILE_TRAIN = "sample_category_img_train.txt"
 SAMPLE_CATEGORY_IMG_FILE_VALIDATION = "sample_category_img_validation.txt"
 
-def parse_images(filename):
+BATCH_SIZE = 20
+
+def read_img_file(filename):
+    with open(filename, 'r') as tsv_f:
+        rows = (line.split() for line in tsv_f.readlines())
+        return [(img, int(cat)-1) for img, cat in rows]
+
+def process_images(table, batch=None):
     image_reader = tf.WholeFileReader()
 
     images = []
     labels = []
-    with open(filename, 'r') as tsv_f:
-        for line in tsv_f.readlines():
-            imgfile, cat = line.split('\t')
-            labels.append(int(cat) - 1)
 
-            imgfile = os.path.join(DATA_DIRECTORY, imgfile)
-            _, image_f = image_reader.read(tf.train.string_input_producer([imgfile]))
-            image = tf.image.decode_jpeg(image_f, channels=3)
-            resized_image = tf.image.resize_images(image, RESCALE_SIZE)
-            images.append(resized_image)
+    sub_table = table[batch:batch+BATCH_SIZE] if batch is not None else table
+    for imgfile, cat in sub_table:
+        full_path = os.path.join(DATA_DIRECTORY, imgfile)
+        _, image_f = image_reader.read(tf.train.string_input_producer([full_path]))
+        image = tf.image.decode_jpeg(image_f, channels=3)
+        resized_image = tf.image.resize_images(image, RESCALE_SIZE)
+        images.append(resized_image)
+
+        labels.append(cat)
 
     return tf.convert_to_tensor(images), tf.convert_to_tensor(labels)
-    # return tf.train.batch(images, 100), tf.train.batch(labels, 100)
 
 def main(argv):
-    # Create the Estimator
-    top_bottom_classifier = tf.estimator.Estimator(model_fn=cnn_vgg16.vgg16, model_dir="/tmp/top_bottom_convnet_model")
+    top_bottom_classifier = tf.estimator.Estimator(
+            model_fn=cnn_vgg16.vgg16,
+            model_dir="/tmp/top_bottom_convnet_model")
 
     # Set up logging for predictions
     # Log the values in the "Softmax" tensor with label "probabilities"
@@ -42,13 +49,19 @@ def main(argv):
           tensors=tensors_to_log, every_n_iter=50)
 
     if len(argv) > 1 and argv[1] == '-t':
-        top_bottom_classifier.train(
-                input_fn=lambda: parse_images(SAMPLE_CATEGORY_IMG_FILE_TRAIN),
-                steps=20000,
-                hooks=[logging_hook])
+        training_table = read_img_file(SAMPLE_CATEGORY_IMG_FILE_TRAIN)
+
+        batch = 0
+        while batch < len(training_table):
+            print("TRAINING batch %d" % batch)
+            top_bottom_classifier.train(
+                    input_fn=lambda: process_images(training_table, batch),
+                    hooks=[logging_hook])
+
+    evaluation_table = read_img_file(SAMPLE_CATEGORY_IMG_FILE_VALIDATION)
 
     eval_results = top_bottom_classifier.evaluate(
-            input_fn=lambda: parse_images(SAMPLE_CATEGORY_IMG_FILE_VALIDATION))
+            input_fn=lambda: process_images(evaluation_table))
 
     print(eval_results)
 
